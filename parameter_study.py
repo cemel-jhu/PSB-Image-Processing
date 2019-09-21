11dbe9773b2343590098a000d4c74eec0354d1de
Dylan Madisetti <madisetti@jhu.edu>
2019-09-21 18:43:16 -0400

    Notebook import validated

--- parameter_study.py
+++ parameter_study.py
@@ -1,30 +1,28 @@
 import import_ipynb
 import PSM_Processing as nb
 
-
-def run(band,
+def run_study(index, band,
         scale=nb.scale,
         frequency=nb.frequency,
         direction=nb.direction,
         sigma=nb.sigma,
-        beam_height=nb.beam_height,
-        beam_width=nb.beam_width,
+        height=nb.beam_height,
+        width=nb.beam_width,
         flip=nb.flip,
         time_margin=nb.time_margin,
-        bands=nb.bands):
-  print(band)
+        bands=None):
+  pass
 
 
 def run():
-  return
   for (key, experiment) in nb.experiments.items():
-    for band in experiments.bands:
+    for (index, band) in experiment.bands.items():
       # We can't extract velocity data is the segment to crop is not defined.
       if band.lower == band.upper:
         continue
       data = experiment._asdict().copy()
       # change parameters in data
-      run_study(band, **data)
+      run_study(index, band, **data)
 
 
 if __name__ == "__main__":
